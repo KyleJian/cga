392883f73c1d2e5a6ed07289dc3b1cc3cd675c2a
KyleJian <kjian@uwo.ca>
2024-05-04 11:27:25 -0400

    ok

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-key = "AIzaSyCtiZuySby0S1CQLNXsENH38I_KP1H2LWY"
-
-genai.configure(api_key=key)
+key = "AIzaSyApYB0fyfPBBiEK_EmV78VS0ixDG_nSIpY"
+print(os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key="AIzaSyApYB0fyfPBBiEK_EmV78VS0ixDG_nSIpY")
+print(genai.configure)
 
 # read all pdf files and return text
 
@@ -39,20 +40,25 @@ def get_text_chunks(text):
 
 
 def get_vector_store(chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=key)  # type: ignore
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001", google_api_key=key)  # type: ignore
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
 
 def get_conversational_chain():
     prompt_template = """
-    Summarize the text
+    Summerize the text\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
-                                   temperature=0.30, google_api_key=key
-                                   )
+                                   temperature=0.3,
+                                   google_api_key=key)
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
     chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
@@ -66,26 +72,25 @@ def clear_chat_history():
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",google_api_key=key)  # type: ignore
+        model="models/embedding-001", google_api_key=key)  # type: ignore
 
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
 
-    response = chain(
-        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True, )
 
-    # print(response)
+    print(response)
     return response
 
 
 def main():
     st.set_page_config(
-        page_title="PDF Summarizer",
+        page_title="Gemini PDF Chatbot",
         page_icon="🤖"
     )
-
+    pdf= None
     # Sidebar for uploading PDF files
     with st.sidebar:
         st.title("Menu:")
@@ -94,13 +99,14 @@ def main():
         if st.button("Submit & Process"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
+                pdf = pdf_docs
+                print(pdf)
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
 
-
     # Main content area for displaying chat messages
-    st.title("Summarize PDF files using Gemini🤖")
+    st.title("Chat with PDF files using Gemini🤖")
     st.write("Welcome to the chat!")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
@@ -115,15 +121,16 @@ def main():
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
-    st.session_state.messages.append({"role": "user", "content": "summarize this text"})
-    # with st.chat_message("user"):
-    #     st.write("summarize")
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
     # Display chat messages and bot response
-    if st.session_state.messages[-1]["role"] != "assistant":
+    if st.session_state.messages[-1]["role"] == "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = user_input("prompt")
+                response = user_input(prompt)
                 placeholder = st.empty()
                 full_response = ''
                 for item in response['output_text']:
